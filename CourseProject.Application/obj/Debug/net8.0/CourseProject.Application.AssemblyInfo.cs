adadfa4c9e6a2a015ac62d258c95a40053290c30
Roslan <rs1409089988@gmail.com>
2024-10-25 21:01:28 +0500

    update database

--- CourseProject.Application/obj/Debug/net8.0/CourseProject.Application.AssemblyInfo.cs
+++ CourseProject.Application/obj/Debug/net8.0/CourseProject.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseProject.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9ce1eafab7e51fa2002d2e259a5314ca61f27a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6a834c89681bd15289fd5a3125ee4eded4d09f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseProject.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseProject.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
