39d37ab2d4ce49be875d1158b24f4b5e3851b4c1
Roslan <rs1409089988@gmail.com>
2024-10-28 08:48:12 +0500

    fix delpoy

--- CourseProject.DataAccess/Hubs/CommentHub.cs
+++ CourseProject.DataAccess/Hubs/CommentHub.cs
@@ -1,6 +1,32 @@
-namespace CourseProject.DataAccess.Hubs;
+using System.Security.Claims;
+using CourseProject.Domain.Abstractions.IServices;
+using CourseProject.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 
-public class CommentHub
+namespace CourseProject.DataAccess.Hubs;
+//(ICommentsService commentsService, UserManager<ApplicationUser> userManager)
+/*var user = await userManager.FindByNameAsync(userName);
+var comment = new Comment
 {
-    
+    Text = commentText,
+    ApplicationUserId = user.Id,
+    FormTemplateId = formTemplateId
+};
+await commentsService.CreateCommentAsync(comment);*/
+public class CommentHub(ICommentsService commentsService, UserManager<ApplicationUser> userManager) : Hub
+{
+    public async Task SendComment(string userName, string commentText, Guid formTemplateId)
+    {
+        if (string.IsNullOrWhiteSpace(commentText)) return;
+        var user = await userManager.FindByNameAsync(userName);
+        var comment = new Comment
+        {
+            Text = commentText,
+            ApplicationUserId = user.Id,
+            FormTemplateId = formTemplateId
+        };
+        await commentsService.CreateCommentAsync(comment);
+        await Clients.All.SendAsync("ReceiveComment", $"{userName} {(comment.CreatedAt.ToLocalTime())}", commentText);
+    }
 }
--- CourseProject.DataAccess/obj/Debug/net8.0/CourseProject.DataAccess.AssemblyInfo.cs
+++ CourseProject.DataAccess/obj/Debug/net8.0/CourseProject.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseProject.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1fff527d262cf88b3628189aaa7ff9cb006ca01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26b29d4b220175bdcd8840af872889d2cd64c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseProject.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseProject.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
